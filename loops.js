eda5f0f41fa176c7342ec882a34bc9aefdcec354
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-19 02:58:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,8 @@ function whileLoop(n) {
  
  function doWhileLoop(array) {
    function maybeTrue() {
-     return Math.random()
+     return Math.random() >= 0.5
+   }
      do {
        array.pop();
      } while (array.length > 1 && )
