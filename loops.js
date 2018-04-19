1d1808579d4ee183a479409af2c13544fb3fa23e
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-19 02:57:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function whileLoop(n) {
  
  function doWhileLoop(array) {
    function maybeTrue() {
-     
+     return Math.random()
      do {
        array.pop();
      } while (array.length > 1 && )
