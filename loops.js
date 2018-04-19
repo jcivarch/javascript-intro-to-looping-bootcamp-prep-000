ac1303c6dfc431af84c6df506696d90830ce1526
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-19 02:58:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,6 +24,7 @@ function whileLoop(n) {
    }
      do {
        array.pop();
-     } while (array.length > -1 && maybeTrue())
+     } while (array.length > -1 && maybeTrue());
    }
+   return 
  }
